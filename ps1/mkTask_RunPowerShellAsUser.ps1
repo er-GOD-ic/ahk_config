64e193ea496acdf7c0da1ae8fed9faa4cd903274
ergodic <kamepower@outlook.jp>
2026-04-15 05:51:23 +0900

    MultipleInstances

--- ps1/mkTask_RunPowerShellAsUser.ps1
+++ ps1/mkTask_RunPowerShellAsUser.ps1
@@ -1,5 +1,18 @@
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -WorkingDirectory $env:USERPROFILE
-$principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive -RunLevel Limited
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd
 
-Register-ScheduledTask -TaskName "RunPowerShellAsUser" -Action $action -Principal $principal -Settings $settings
+$principal = New-ScheduledTaskPrincipal `
+    -UserId $env:USERNAME `
+    -LogonType Interactive `
+    -RunLevel Limited
+
+$settings = New-ScheduledTaskSettingsSet `
+    -AllowStartIfOnBatteries `
+    -DontStopIfGoingOnBatteries `
+    -DontStopOnIdleEnd1
+    -MultipleInstances Parallel
+
+Register-ScheduledTask `
+    -TaskName "RunPowerShellAsUser" `
+    -Action $action `
+    -Principal $principal `
+    -Settings $settings
